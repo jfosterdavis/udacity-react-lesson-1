a553e1d7c7e85c445afc6fe8f2bc14c0ba1e737e
J. Foster Davis <jfosterdavis@users.noreply.github.com>
2020-06-10 21:21:54 +0000

    my answer after seeing udacity answer

--- src/ListUserFavoriteMovies.js
+++ src/ListUserFavoriteMovies.js
@@ -9,31 +9,28 @@ class ListUserFavoriteMovies extends Component {
     	const users = this.props.users
         const profiles = this.props.profiles
         const movies = this.props.movies
-        let favMovieId = null
 		console.log('Profiles', profiles);
 		return (
 				<ol className='user-movie-list'>
-					{Object.entries(users).map(([key, user])=> (
+					{profiles.map( profile => {
+          				const user = users[profile.userID];
+        				const favMovie = movies[profile.favoriteMovieID];
+    
+    					return(
 						<li key={user.id} className={'user-list-item'}>
 							<div className='user-details' >
 								<p>{user.name}</p>
 								<p>{user.userName}</p>
 								<p>{user.id}</p>
 
+							
 							</div>
-							<div className='favorite-movie'>
-                              {console.log('user id',user.id)}
-								
-{Object.entries(movies).filter(([key, movie]) => (movie.id == profiles.filter(profile => (profile.userID == user.id))[0].favoriteMovieID)).map(([key, movie]) => (
-  <div key={movie.id+user.id}>{movie.name}</div>
-								
-                                  
-                                  
-									
-                              ))}
-							</div>
+                            <div className='movie-details' >
+                            <p>{favMovie.name}</p>
+                            </div>
 						</li>
-					))}
+							)
+					})}
 				</ol>
 				)
 	}
